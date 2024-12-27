e4d5cebe5f98f9253ee93465cb73e21a9b281b81
AEmreEser <emre.eser@sabanciuniv.edu>
2024-12-27 12:51:26 +0000

    comment cleanup

--- kmeans.cu
+++ kmeans.cu
@@ -30,14 +30,12 @@ using namespace std;
 /** TODO:
 done: --1 do the convergence check thing 
 failed: --2 use n*k threads and parallelize the for loop in assignKernel -- for large k values this is the bottleneck currently
---3 Join the kernels into one big kernel and do everything on the gpu, don't lose time communicating
---4 Use the cuda timer stuff for measuring walltimes - not the omp walltime thing
+canceled: --3 Join the kernels into one big kernel and do everything on the gpu, don't lose time communicating
+done:--4 Use the cuda timer stuff for measuring walltimes - not the omp walltime thing
 done:-.5 use Harris reduction on convergence check
---6 comment cleanup
+done:--6 comment cleanup
 --7 two gpus
 **/
-
-// #define DEBUG
 
 __host__ __device__ double dist(double x1, double y1, double x2, double y2) {
     return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);  // sqrt is omitted as it reduces performance.
@@ -47,7 +45,7 @@ __global__ void assignKernel(int *x, int *y, int *c, double *cx, double *cy, /* 
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
         int cluster;
-        double minDist = (INT_MAX); // static_cast<double>(INT_MAX); // not using casting saves a miniscule amount of time
+        double minDist = (INT_MAX); 
         // for really large values of k, the for loop creates a huge bottleneck
         for (int j = 0; j < k; j++) {
             double distance = dist(cx[j], cy[j], x[idx], y[idx]);
@@ -55,57 +53,12 @@ __global__ void assignKernel(int *x, int *y, int *c, double *cx, double *cy, /* 
                 minDist = distance;
                 cluster = j;
             }
-            // these take more time than the if check above - probably because of the conversion and the multiplication
-            // cluster = (j * (distance < minDist)) + (cluster * (!(distance < minDist)));
-            // minDist = distance * (distance < minDist) + minDist * (!(distance < minDist));
         }
 
-        // changed[idx] = (c[idx] != cluster);
-        // printf("idx=%d, c[idx]=%d, cluster=%d, changed[idx]=%d\n", idx, c[idx], cluster, changed[idx]);
         c[idx] = cluster; // assign the point to the cluster with minDist
-        // __threadfence();
 
     }
 }
-
-// __global__ void assignKernelFullyParallel(int *x, int *y, int *c, double *cx, double *cy, double* all_dists, int* all_assignments, int k, int n) {
-//     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//     int maxthreads = n*k;
-//     if(idx >= maxthreads){ return; }
-
-//     int kidx = threadIdx.x; int nidx = blockIdx.x;
-
-//     // extern __shared__ double dists_to_centroids[]; // size == k*sizeof(double) + k*sizeof(long long int)
-
-//     // dists_to_centroids[kidx] = dist(x[nidx], y[nidx], cx[kidx], cy[kidx]); 
-//     // dists_to_centroids[k + kidx] = kidx;
-//     all_dists[idx] = dist(x[nidx], y[nidx], cx[kidx], cy[kidx]); 
-//     all_assignments[idx] = -1;
-
-//     __syncthreads();
-
-//     for(long long int s=1; s < k; s*=2){ // assume k is divisible by 2
-//         if(kidx % (2*s) == 0){
-//             // if(dists_to_centroids[kidx] > dists_to_centroids[kidx + s]){
-//             //     dists_to_centroids[kidx] = dists_to_centroids[kidx + s]; // min reduction
-//             //     dists_to_centroids[k + kidx] = __longlong_as_double(s);  // total goddamn waste
-//             // } // if
-//             printf("idx=%d, all_dists[idx]=%f, all_dists[idx+s]=%f\n", idx ,all_dists[idx], all_dists[idx+s]);
-//             if (all_dists[idx] > all_dists[idx+s]){
-//                 all_dists[idx] = all_dists[idx+s];
-//                 all_assignments[idx] = static_cast<int>(s);
-//             }
-//             printf("nidx=%d, kidx=%d, dists=%f, kidx+s=%d, dists=%f\n", nidx, kidx, all_dists[kidx], kidx+s, all_dists[kidx+s]);
-//         } // if %
-//         __syncthreads();
-//     } // for
-
-//     if (kidx == 0){
-//         c[nidx] = (all_assignments[idx]);
-//         printf("nidx=%d, c[nidx]=%d\n", nidx, c[nidx]);
-//     }
-
-// }
 
 // single block, k many threads
 __global__ void check_cluster_change(double *cx, double *cy, double *prev_cx, double *prev_cy, bool* changed, bool *red_change, int k) {
@@ -114,24 +67,12 @@ __global__ void check_cluster_change(double *cx, double *cy, double *prev_cx, do
 
     if (threadId == 0){ *red_change = false; }
 
-    // bool local_cont = false;
-
-    // Each thread checks one element
-    // if (threadId < k) {
-        // local_cont = (cx[threadId] != prev_cx[threadId]) || (cy[threadId] != prev_cy[threadId]);
-    // not that big a reduction in perf since global mem accesses are coalesced
     changed[threadId] = (cx[threadId] != prev_cx[threadId]) || (cy[threadId] != prev_cy[threadId]);
-    // }
 
-    // Store the local result in shared memory
-    // shared_cont[threadIdx.x] = local_cont;
     __syncthreads();
 
-    // Perform parallel reduction in shared memory using OR operation
-    // FOR NOW ASSUME THAT K IS AN INTEGER MULTIPLE OF 2
     for (int s = 1; s < k; s *= 2) { 
         int index = 2 * s * threadId;
-        // if (threadId % (2 * s) == 0) { // highly divergent
         if(index < k){ // not divergent 
             changed[threadId] |= changed[threadId + s];
         }
@@ -150,9 +91,7 @@ __global__ void check_cluster_change(double *cx, double *cy, double *prev_cx, do
 
     // The first thread in each block writes the result to global memory
     if (threadId == 0) {
-        // atomicOr(cont, changed[0]);
         *red_change |= changed[0];
-        // printf("Post Reduction *red_change=%d\n", *red_change);
     }
 
 }
@@ -164,19 +103,7 @@ __global__ void updateKernel(int *x, int *y, int *c, int k, int n, double *sumx,
         atomicAdd(&sumx[c[idx]], x[idx]);
         atomicAdd(&sumy[c[idx]], y[idx]);
         atomicAdd(&count[c[idx]], 1);
-        // __syncthreads();
     }
-    // reduction on changed[]:
-    // sub-optimal proof of concept version:
-    // __syncthreads();
-    // if (idx == 0){
-    //     *cont = false;
-    //     for (int i = 0; i < n; i++){
-    //         *cont |= changed[idx];    
-    //         // printf("%d: changed[idx]=%d\n", i, changed[idx]);
-    //         if (*cont == true){ break; }
-    //     }
-    // }
 }
 
 __global__ void computeCentroids(int k, double *sumx, double *sumy, int *count, double *cx, double *cy) {
@@ -185,7 +112,12 @@ __global__ void computeCentroids(int k, double *sumx, double *sumy, int *count, 
         if (count[idx] > 0) {
             cx[idx] = sumx[idx] / count[idx];
             cy[idx] = sumy[idx] / count[idx];
-        }
+        } 
+        #ifdef DEBUG
+            else {
+                printf("centroid %d: count=0\n", idx);
+            }
+        #endif
         // for some reason these affect performance badly, probably due to the multiplications
         // cx[idx] = (sumx[idx] / count[idx]) * (count[idx] > 0) + cx[idx] * (count[idx] <= 0);
         // cy[idx] = (sumy[idx] / count[idx]) * (count[idx] > 0) + cy[idx] * (count[idx] <= 0);
@@ -250,11 +182,7 @@ void kmeans(int *x, int *y, int *c, double *cx, double *cy, int k, int n) {
 
     int *d_x, *d_y, *d_c;
     double *d_cx, *d_cy, *d_sumx, *d_sumy; 
-    // double *d_all_dists;
-    // int *d_all_assignments;
     int *d_count;
-    // bool *cont;
-    // cont = new bool;
     bool cont;
     #ifdef HOSTREDUCE
         double *prev_cx, *prev_cy;
@@ -274,8 +202,6 @@ void kmeans(int *x, int *y, int *c, double *cx, double *cy, int k, int n) {
         cudaMalloc(&d_prev_cy, k * sizeof(double));
         cudaMalloc(&d_changed, k * sizeof(bool));
 
-        // cudaMemset(d_prev_cx, -1.0f, k*sizeof(double));
-        // cudaMemset(d_prev_cy, -1.0f, k*sizeof(double));
         cudaMemset(d_changed, false, k*sizeof(bool));
         cudaMemset(d_red_change,    false, 1*sizeof(bool));
     #endif
@@ -288,10 +214,6 @@ void kmeans(int *x, int *y, int *c, double *cx, double *cy, int k, int n) {
     cudaMalloc(&d_sumx, k * sizeof(double));
     cudaMalloc(&d_sumy, k * sizeof(double));
     cudaMalloc(&d_count, k * sizeof(int));
-    // cudaMalloc(&d_all_dists, n*k* sizeof(double));
-    // cudaMalloc(&d_all_assignments, n*k* sizeof(int));
-    // cudaMalloc(&d_changed, n * sizeof(bool));
-    // cudaMalloc(&d_cont, 1 * sizeof(bool));
     
     cudaMemcpy(d_x, x, n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_y, y, n * sizeof(int), cudaMemcpyHostToDevice);
@@ -301,9 +223,7 @@ void kmeans(int *x, int *y, int *c, double *cx, double *cy, int k, int n) {
 
     int blockSize = 256;
     int gridSize = (n + blockSize - 1) / blockSize;
-    cudaMemset(d_c, -1, n * sizeof(int)); // set to some invalid cluster so that every pt gets a cluster change in the first iter
-    // cudaMemset(d_all_dists, -1.0f, n*k* sizeof(double)); // set to some invalid cluster so that every pt gets a cluster change in the first iter
-    // cudaMemset(d_all_assignments, -1, n*k* sizeof(int)); // set to some invalid cluster so that every pt gets a cluster change in the first iter
+    cudaMemset(d_c, -1, n * sizeof(int)); 
 
     while (iter < MAX_ITER) {
         #ifdef DEBUG
@@ -311,13 +231,12 @@ void kmeans(int *x, int *y, int *c, double *cx, double *cy, int k, int n) {
         #endif
         cont = false;
         
-        cudaMemset(d_sumx, 0, k * sizeof(double));
-        cudaMemset(d_sumy, 0, k * sizeof(double));
+        cudaMemset(d_sumx,  0, k * sizeof(double));
+        cudaMemset(d_sumy,  0, k * sizeof(double));
         cudaMemset(d_count, 0, k * sizeof(int));
 
         cudaEventRecord(start, 0);
         assignKernel<<<gridSize, blockSize>>>(d_x, d_y, d_c, d_cx, d_cy, /*d_changed,*/ k, n);
-        // assignKernelFullyParallel<<< (n*k + k - 1)/k, k >>>(d_x, d_y, d_c, d_cx, d_cy, d_all_dists, d_all_assignments, /*d_changed,*/ k, n);
         cudaDeviceSynchronize();
         cudaEventRecord(stop, 0);
         cudaEventSynchronize(stop);
@@ -464,7 +383,6 @@ int readfile(const string& fname, int*& x, int*& y) {
         y = new int[n];
     #endif
 
-    // Read the points
     int tempx, tempy;
     for (int i = 0; i < n; i++) {
         getline(f, line);
@@ -507,8 +425,6 @@ int main(int argc, char *argv[]) {
     // Measure k-means execution time
     double kmeans_start = omp_get_wtime();
     kmeans(x, y, c, cx, cy, k, n);
-
-    // cudaDeviceSynchronize(); // sync inside kmeans func
 
     double kmeans_end = omp_get_wtime();
     printf("K-Means Execution Time: %f seconds\n", kmeans_end - kmeans_start);
